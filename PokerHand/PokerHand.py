5ff230991879e0d800d96c4269ec3dd32dfb5a36
sonjoonho <json0820@gmail.com>
2018-11-01 20:56:53 +0000

    Added class representing the value of a hand

--- PokerHand/PokerHand.py
+++ PokerHand/PokerHand.py
@@ -5,17 +5,17 @@ hands.
 
 """
 class PokerHand:
-    def __init__(self, hand):
+    def __init__(self, cards):
         try:
             # TODO check input format strictly, first character of each card
             # is the value, second is the suit
-            self.hand = hand.split()
+            self.cards = cards.split()
 
-            if len(self.hand) != 5:
+            if len(self.cards) != 5:
                 raise ValueError("Invalid format: Hands must consist of "
                                  "exactly 5 cards.")
 
-            self.score = self._calculate_score(self.hand)
+            self.value = self._calculate_value()
 
         except (AttributeError, TypeError):
             print("Invalid format: Hand must be a string.")
@@ -32,8 +32,24 @@ class PokerHand:
 
         return Result.TIE
 
-    def _calculate_score(self):
+    def _calculate_value(self):
         pass
+
+class value:
+    """
+    Represents the value of a hand. Implemented according to as detailed by 
+    https://en.wikipedia.org/wiki/Texas_hold_%27em#Hand_values. 
+    """
+    HIGHCARD = 0
+    PAIR = 1
+    TWO_PAIRS = 2
+    THREE_OF_A_KIND = 3
+    STRAIGHT = 4
+    FLUSH = 5
+    FULL_HOUSE = 6
+    FOUR_OF_A_KIND = 7
+    STRAIGHT_FLUSH = 8
+    ROYAL_FLUSH = 9
 
 
 class Result:
