261db5b68bfe6fc38794b5f022c6df2909bab6f5
sonjoonho <json0820@gmail.com>
2018-11-01 20:22:28 +0000

    Implemented basic tokenisation and input checking

--- PokerHand/PokerHand.py
+++ PokerHand/PokerHand.py
@@ -1,10 +1,40 @@
+"""
+Implements a basic PokerHand object that encapsulates the cards that the hand
+consists of, and a function to compare the value of the hand against other
+hands.
+
+"""
 class PokerHand:
     def __init__(self, hand):
-        self.hand = hand
+        try:
+            # TODO check input format strictly, first character of each card
+            # is the value, second is the suit
+            self.hand = hand.split()
+
+            if len(self.hand) != 5:
+                raise ValueError("Invalid format: Hands must consist of "
+                                 "exactly 5 cards.")
+
+            self.score = self._calculate_score(self.hand)
+
+        except (AttributeError, TypeError):
+            print("Invalid format: Hand must be a string.")
+
 
     def compareWith(self, other_hand):
+        """
+        Args:
+            other_hand A PokerHand to be compared with.
+
+        Returns:
+            The result of the comparison.
+        """
 
         return Result.TIE
+
+    def _calculate_score(self):
+        pass
+
 
 class Result:
     WIN = 1
